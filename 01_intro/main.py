6aa17b82eccbdc59f71426e9e0d62bf9e92ac3f5
Ayush Bommana <bommanaayush07@gmail.com>
2026-06-14 10:49:56 +0530

    feat: Create the basic starter app

---
+++ 01_intro/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from scalar_fastapi import get_scalar_api_reference
+
+app= FastAPI()
+
+@app.get("/")
+async def get_home():
+    return {"Hello": "World"}
+
+# scalar docs
+@app.get("/scalar", include_in_schema= False)
+async def scalar_html():
+    return get_scalar_api_reference(
+        openapi_url=app.openapi_url,
+        title= "My First API",
+    )
+
+@app.get("/shipment")
+async def get_shipment():
+    return {"id": "123", "status": "delivered"}
